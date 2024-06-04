73f49b5ef30145f49332b7871a9b92752f1d11e0
Vishal Anand Muruganandam <45725769+Vishal921999@users.noreply.github.com>
2024-06-04 16:08:22 -0700

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Container from './components/Container/Container';
+import DataTable from './components/DataTable/DataTable';
+
+interface IAppProps {
+}
+
+const App: React.FunctionComponent<IAppProps> = () => {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
